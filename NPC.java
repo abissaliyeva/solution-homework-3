81916e71181c008270e59b45bce3d0b81b97a100
abissaliyeva <aruzhan.abissaliyeva@narxoz.kz>
2025-03-04 19:39:53 +0600

    NPC.java

--- NPC.java
+++ NPC.java
@@ -1,4 +1,13 @@
 package assignment1;
 
 public class NPC {
+    private String name;
+
+    public NPC(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
