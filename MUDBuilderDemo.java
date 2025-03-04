e4e030be60431b5ff1d57466bafe1c3b8ab4cfff
abissaliyeva <aruzhan.abissaliyeva@narxoz.kz>
2025-03-04 19:39:28 +0600

    MUDBuilderDemo.java

--- MUDBuilderDemo.java
+++ MUDBuilderDemo.java
@@ -1,5 +1,15 @@
 package assignment1;
 
-public class MUDBuilderDemo {
+import mud.Room;
 
+public class MUDBuilderDemo {
+    public static void main(String[] args) {
+        IDungeonBuilder builder = new SimpleDungeonBuilder();
+        Room room1 = new Room("room","some random room");
+        NPC npc = new NPC("npc");
+
+        Dungeon dungeon = builder.setName("dungeon").addRoom(room1).addNPC(npc).build();
+
+        dungeon.show();
+    }
 }
