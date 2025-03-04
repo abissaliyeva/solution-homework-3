53e2e4425a8075739cd0ffe7341ec8bdc4861ab5
abissaliyeva <aruzhan.abissaliyeva@narxoz.kz>
2025-03-04 19:40:15 +0600

    SimpleDungeonBuilder.java

--- SimpleDungeonBuilder.java
+++ SimpleDungeonBuilder.java
@@ -1,20 +1,31 @@
 package assignment1;
 
 import mud.Room;
-import assignment1.Dungeon;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SimpleDungeonBuilder implements IDungeonBuilder {
-    public void setName(String name) {
-        this.name = name;
+    private Dungeon dungeon;
+    private List<Room> rooms = new ArrayList<>();
+    private List<NPC> npcs = new ArrayList<>();
+
+    public SimpleDungeonBuilder() {
+        this.dungeon = new Dungeon();
     }
-    public String getName() {
-        return name;
+    public IDungeonBuilder setName(String name) {
+        dungeon.setName(name);
+        return this;
     }
-    public void addRoom(Room room) {
-        rooms.add(room);
+    public IDungeonBuilder addRoom(Room room) {
+        dungeon.addRooms(room);
+        return this;
     }
-    public void addNPC(NPC npc) {
-        npcs.add(npc);
+    public IDungeonBuilder addNPC(NPC npc) {
+        dungeon.addNpcs(npc);
+        return this;
     }
-    public
+    public Dungeon build() {
+        return dungeon;
+    }
 }
